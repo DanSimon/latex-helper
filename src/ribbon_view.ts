fa212d4a79d61d0b2b60c410c73252b0544e52c8
Dan Simon <dansimon101@gmail.com>
2025-01-12 10:38:45 -0500

    upgrade node

--- src/ribbon_view.ts
+++ src/ribbon_view.ts
@@ -1,6 +1,6 @@
 import { ItemView, WorkspaceLeaf } from "obsidian";
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
 import { ConfigManager } from "./config";
 import RibbonViewComponent from "./components/RibbonViewComponent";
 
@@ -8,7 +8,8 @@ export const RIBBON_VIEW_TYPE = "unified-latex-view";
 
 export class RibbonView extends ItemView {
     private configManager: ConfigManager;
-    private root: Element | null = null;
+    private reactRoot: ReactDOM.Root | null = null;
+    private containerElement: Element | null = null;
 
     constructor(leaf: WorkspaceLeaf, configManager: ConfigManager) {
         super(leaf);
@@ -37,28 +38,33 @@ export class RibbonView extends ItemView {
             this.renderComponent();
         });
 
-        if (this.root) {
-            ReactDOM.unmountComponentAtNode(this.root);
-            this.root = null;
+        // Cleanup React
+        if (this.reactRoot) {
+            this.reactRoot.unmount();
+            this.reactRoot = null;
         }
+
+        this.containerElement = null;
 
         super.onunload();
     }
 
     private renderComponent() {
-        if (!this.root) return;
+        if (!this.containerElement || !this.reactRoot) return;
 
-        ReactDOM.render(
+        this.reactRoot.render(
             React.createElement(RibbonViewComponent, {
                 view: this,
                 configManager: this.configManager,
             }),
-            this.root,
         );
     }
 
     async onOpen() {
-        this.root = this.containerEl.children[1];
-        this.renderComponent();
+        this.containerElement = this.containerEl.children[1];
+        if (this.containerElement) {
+            this.reactRoot = ReactDOM.createRoot(this.containerElement);
+            this.renderComponent();
+        }
     }
 }
